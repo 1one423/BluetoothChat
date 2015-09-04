0fd6db06cc4d331fac81b840170433c962f063b2
lijun <likumb@163.com>
2015-09-04 09:49:39 +0800

    项目初始化

---
+++ BluetoothChat/BluetoothChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothChat
+//
+//  Created by  李俊 on 15/9/4.
+//  Copyright (c) 2015年  李俊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
